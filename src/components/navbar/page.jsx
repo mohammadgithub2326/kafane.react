bca4bf9eaa420b9ff93ce7a93107e836026f3df5
mohammad noor <g.s.dadanoor@gmail.com>
2024-05-01 21:47:05 +0530

    thirdcommit errors solved

--- src/components/navbar/page.jsx
+++ src/components/navbar/page.jsx
@@ -3,15 +3,20 @@
 
 import Link from 'next/link';
 import styles from './navbar.module.css';
-// import { useContext, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Login from '@/app/(Routers)/Login/page';
 
 
 
 const Navbar = () => {
+
+
   const router = useRouter()
+  useEffect(() => {
   const login = localStorage.getItem("loggedIn")
+  }, [])
+  
   
   // console.log(logIn)
   return (
@@ -20,24 +25,24 @@ const Navbar = () => {
       <ul className={styles.navLinks}>
         <li className={styles.navItem}>
            {/* <Link >  */}
-            <p className={styles.navLink}  onClick={() => router.push(`${login ? "/Orders" : "/Login"}`)}
+            <p className={styles.navLink}  onClick={() => router.push("/Orders" )}
 >Orders</p>
           {/* </Link> */}
         </li>
         <li className={styles.navItem}>
           {/* <Link href="/products"> */}
-            <p className={styles.navLink} onClick={() => router.push(`${login ? "/products" : "/Login"}`)}
+            <p className={styles.navLink} onClick={() => router.push("/products")}
 >Products</p>
           {/* </Link> */}
         </li>
         <li className={styles.navItem}>
           {/* <Link href="/Users" > */}
-            <p className={styles.navLink} onClick={() => router.push(`${login ? "/Users" : "/Login"}`)} >Users</p>
+            <p className={styles.navLink} onClick={() => router.push("/Users" )} >Users</p>
           {/* </Link> */}
         </li>
         <li className={styles.navItem}>
           <Link href="/Login">
-            <p className={styles.navLink} onClick={()=>localStorage.setItem("loggedIn",false)}>Logout</p>
+            <p className={styles.navLink} >Logout</p>
           </Link>
         </li>
       </ul>
